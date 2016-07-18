fdb4d929ee4dbfb2192427df861628b283fac4cf
ruedap <ruedap@ruedap.com>
2016-07-18 17:36:42 +0900

    Add Logo component

---
+++ src/components/atoms/Logo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import styles from './Logo.css'
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <img src="static/images/logo_ruedapcom.svg" alt="ルエダップコム" />
+    );
+  }
+}
--- src/components/templates/Page.js
+++ src/components/templates/Page.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import styles from './Page.css'
 import Avatar from '../atoms/Avatar';
+import Logo from '../atoms/Logo';
 
 export default class Page extends Component {
   render() {
     return (
       <div className={styles.base}>
         <Avatar />
+        <Logo />
       </div>
     );
   }
