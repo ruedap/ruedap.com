2435fe78313dde1c5b01be1f71bf0b68976c4e73
ruedap <ruedap@ruedap.com>
2016-07-18 14:36:48 +0900

    Update webpack.config.js for CSS Modules

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,16 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css?modules&localIdentName=[name]-[local]---[hash:base64:21]'],
+      },
+    ]
   }
 };
