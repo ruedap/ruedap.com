a467760fe95277657cd1597eb3f0e6d86fd39be6
ruedap <ruedap@ruedap.com>
2016-07-18 22:42:52 +0900

    Convert to json format in product data

--- src/components/molecules/Product.js
+++ src/components/molecules/Product.js
@@ -3,26 +3,30 @@ import styles from './Product.css'
 
 export default class Product extends Component {
   render() {
+    const html = { __html: this.props.description };
     return (
       <article className={styles.root_wideImage}>
-        <h1 className={styles.name}>アインシュタインの電話番号</h1>
-        <a className={styles.imageLink} href="#">
-          <img className={styles.image} src="static/images/products/daplog.png" alt="アインシュタインの電話番号"
+        <h1 className={styles.name}>{this.props.name}</h1>
+        <a className={styles.imageLink} href={this.props.imageLink}>
+          <img className={styles.image} src={this.props.image} alt={this.props.name}
  />
         </a>
         <div className={styles.body}>
-          <time className={styles.time} pubdate="2013-11-11">2013.11.11</time>
+          <time className={styles.time} pubdate={this.props.pubdate}>
+            {this.props.time}
+          </time>
           <h2 className={styles.heading}>
-            <a className={styles.headingLink} href="#">
-              Pull RequestとCIを使ったGitHub Flowなブログ環境を作ってみた
+            <a className={styles.headingLink} href={this.props.headingLink}>
+              {this.props.heading}
             </a>
           </h2>
-          <div className={styles.description}>
-            今から1年ほど前に、自分でお気に入りのブログ環境を構築する記事が話題になっていて、それを読んだ時から、自分用のブログ環境を作りたいと思っていた。
-          </div>
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={html}
+          />
         </div>
         <nav className={styles.more}>
-          <a className={styles.moreLink} href="#">
+          <a className={styles.moreLink} href={this.props.headingLink}>
             続きを読む
           </a>
         </nav>
