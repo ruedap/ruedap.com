6724cd8aef1d4035cc773bb4bd8f19bfd68845af
ruedap <ruedap@ruedap.com>
2016-07-20 10:36:13 +0900

    Convert ProfileLinkList data to json

--- src/components/atoms/ProfileLink.js
+++ src/components/atoms/ProfileLink.js
@@ -4,7 +4,7 @@ import styles from './ProfileLink.css'
 export default class ProfileLink extends Component {
   render() {
     return (
-      <a className={styles.root} href={this.props.href} target="_blank">
+      <a className={styles.root} href={this.props.href}>
         {this.props.name}
       </a>
     );
