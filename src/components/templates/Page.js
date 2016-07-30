a66a27a8a3ce7ee2cff764dd4e454de19a8d0534
ruedap <ruedap@ruedap.com>
2016-07-30 18:48:12 +0900

    Replace logo_einstein.svg with Inline SVG

--- src/components/templates/Page.js
+++ src/components/templates/Page.js
@@ -5,6 +5,7 @@ import styles from './Page.css'
 import PageHeader from '../organisms/PageHeader';
 import ProductList from '../organisms/ProductList';
 import SectionTitle from '../atoms/SectionTitle';
+import BlogLogo from '../atoms/BlogLogo';
 
 let cx = classNames.bind(styles);
 
@@ -22,7 +23,9 @@ export default class Page extends Component {
               <SectionTitle image="static/images/section_blog.svg" title="ブログ" />
             </div>
             <a className={styles.blogLink} href="http://blog.ruedap.com/">
-              <img className={styles.blogLogo} src="static/images/logo_einstein.svg" alt="アインシュタインの電話番号" />
+              <div className={styles.blogLogo}>
+                <BlogLogo />
+              </div>
             </a>
           </section>
 
