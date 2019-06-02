bdeca7e005f4f3b7e7457246c23e22660ae72d45
ruedap <ruedap@ruedap.com>
2019-06-02 23:29:00 +0900

    feat(components): Add AtomProduct component

---
+++ components/atoms/AtomProduct.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  article(:class="$style.root")
+    h1(:class="$style.name") {{ name }}
+    a(:class="$style.imageLink" :href="imageLink")
+      img(:class="$style.image" :src="image" :alt="name")
+    div(:class="$style.body")
+      time(:class="$style.time" :dateTime="pubdate") {{ time }}
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: '',
+      required: true
+    },
+    imageLink: {
+      type: String,
+      default: '',
+      required: true
+    },
+    image: {
+      type: String,
+      default: '',
+      required: true
+    },
+    time: {
+      type: String,
+      default: '',
+      required: true
+    },
+    pubdate: {
+      type: String,
+      default: '',
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+.root {
+}
+
+.name {
+}
+
+.imageLink {
+}
+
+.image {
+}
+
+.body {
+}
+.time {
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template lang="pug">
-  div yay
+atom-product(name="アインシュタインの電話番号" imageLink="https://github.com/ruedap/daplog" :image="img" time="2013.11.11" pubdate="2013-11-11")
 </template>
+
+<script>
+import AtomProduct from '~/components/atoms/AtomProduct.vue'
+import img from '~/assets/images/products/daplog.png'
+export default {
+  data() {
+    return {
+      img: img
+    }
+  },
+  components: {
+    AtomProduct
+  }
+}
+</script>
