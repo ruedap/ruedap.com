7085d75397fbabeafd92f63551fe83659d5b0d5e
ruedap <ruedap@ruedap.com>
2019-08-18 19:04:32 +0900

    feat(nuxt): Add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         href: '//fonts.googleapis.com/css?family=PT+Serif:400&subset=latin'
